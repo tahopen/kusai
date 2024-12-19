6320f55b831e6e29d2cb85ba5a6b0d159de3d05a
Sander Silva <sandrewtx08@pm.me>
2024-12-19 20:24:16 -0300

    fix: local lib

---
+++ start.sh
@@ -0,0 +1,23 @@
+mvn install:install-file \
+    -Dfile=./lib/jdk.tools-1.6.jar \
+    -DgroupId=jdk.tools \
+    -DartifactId=jdk.tools \
+    -Dversion=1.6 \
+    -Dpackaging=jar
+
+
+mvn dependency:purge-local-repository
+
+mvn install
+
+mvn install:install-file \
+    -Dfile=./lib/saiku-query-0.4-SNAPSHOT.jar \
+    -DgroupId=org.saiku \
+    -DartifactId=saiku-query \
+    -Dversion=0.4-SNAPSHOT \
+    -Dpackaging=jar
+
+mvn install
+
+./saiku-server/target/dist/saiku-server/set-java.sh
+./saiku-server/target/dist/saiku-server/start-saiku.sh
