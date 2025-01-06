1b4d83ea609475c773aed7493d458a39567966d4
sandrewtx08 <sandrewtx08@protonmail.com>
2025-01-06 16:34:51 -0300

    fix: export pdf memory

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 ./saiku-server/target/dist/saiku-server/set-java.sh
 ./saiku-server/target/dist/saiku-server/stop-saiku.sh
 ./saiku-server/target/dist/saiku-server/start-saiku.sh
+export CATALINA_OPTS="-Xms512m -Xmx1024m -XX:MaxPermSize=128m -Dfile.encoding=UTF-8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Djava.awt.headless=true"
